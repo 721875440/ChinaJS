c51cfa91f451608d19b961842c3300dcbb0e5c7a
jack hang <721875440@qq.com>
2016-10-04 17:49:48 +0800

    1.0

---
+++ China.js
@@ -0,0 +1,26 @@
+Object.prototype.信息框 = function(Str){
+    alert(Str);
+ }
+ 
+ 
+ 
+Object.prototype.文档 = {
+        获取ID:function(id){
+            return document.getElementById(id);
+        }
+ 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
